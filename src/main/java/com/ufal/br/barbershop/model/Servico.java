b381e908f4211af43d168e295490ecac00201bc9
GabrieldeAlmeida96 <45373763+GabrieldeAlmeida96@users.noreply.github.com>
2019-04-01 09:07:54 -0300

    adicionando classe serviço

---
+++ src/main/java/com/ufal/br/barbershop/model/Servico.java
@@ -0,0 +1,27 @@
+package com.ufal.br.barbershop.model;
+
+public class Servico {
+	
+	private String nomeServico;
+	
+	private String codigoServico;
+
+	public String getNomeServico() {
+		return nomeServico;
+	}
+
+	public void setNomeServico(String nomeServico) {
+		this.nomeServico = nomeServico;
+	}
+
+	public String getCodigoServico() {
+		return codigoServico;
+	}
+
+	public void setCodigoServico(String codigoServico) {
+		this.codigoServico = codigoServico;
+	}
+	
+	
+
+}
